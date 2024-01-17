38e676bd4a9fec3b63b6c4cd67207c087e1b2e03
Shawn Catanzarite <me@shawncatz.com>
2024-01-17 02:26:12 -0800

    fork/copy of https://github.com/mrobinsn/go-newznab/tree/master/newznab

---
+++ newznab/newznab.go
@@ -0,0 +1,440 @@
+package newznab
+
+// fork of https://github.com/mrobinsn/go-newznab/tree/master/newznab
+
+import (
+	"crypto/tls"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+// Various constants for categories
+const (
+	// TV Categories
+	// CategoryTVAll is for all shows
+	CategoryTVAll = 5000
+	// CategoryTVForeign is for foreign shows
+	CategoryTVForeign = 5020
+	// CategoryTVSD is for standard-definition shows
+	CategoryTVSD = 5030
+	// CategoryTVHD is for high-definition shows
+	CategoryTVHD = 5040
+	// CategoryTVUHD is for UHD shows
+	CategoryTVUHD = 5045
+	// CategoryTVOther is for other shows
+	CategoryTVOther = 5050
+	// CategoryTVSport is for sports shows
+	CategoryTVSport = 5060
+
+	// Movie categories
+	// CategoryMovieAll is for all movies
+	CategoryMovieAll = 2000
+	// CategoryMovieForeign is for foreign movies
+	CategoryMovieForeign = 2010
+	// CategoryMovieOther is for other movies
+	CategoryMovieOther = 2020
+	// CategoryMovieSD is for standard-definition movies
+	CategoryMovieSD = 2030
+	// CategoryMovieHD is for high-definition movies
+	CategoryMovieHD = 2040
+	// CategoryMovieUHD is for UHD movies
+	CategoryMovieUHD = 2045
+	// CategoryMovieBluRay is for blu-ray movies
+	CategoryMovieBluRay = 2050
+	// CategoryMovie3D is for 3-D movies
+	CategoryMovie3D = 2060
+)
+
+// Client is a type for interacting with a newznab or torznab api
+type Client struct {
+	apikey     string
+	apiBaseURL string
+	apiUserID  int
+	client     *http.Client
+}
+
+// New returns a new instance of Client
+func New(baseURL string, apikey string, userID int, insecure bool) Client {
+	ret := Client{
+		apikey:     apikey,
+		apiBaseURL: baseURL,
+		apiUserID:  userID,
+	}
+	if insecure {
+		ret.client = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}}
+	} else {
+		ret.client = &http.Client{}
+	}
+	return ret
+}
+
+// SearchWithTVRage returns NZBs for the given parameters
+func (c Client) SearchWithTVRage(categories []int, tvRageID int, season int, episode int) ([]NZB, error) {
+	return c.search(url.Values{
+		"rid":     []string{strconv.Itoa(tvRageID)},
+		"cat":     c.splitCats(categories),
+		"season":  []string{strconv.Itoa(season)},
+		"episode": []string{strconv.Itoa(episode)},
+		"t":       []string{"tvsearch"},
+	})
+}
+
+// SearchWithTVDB returns NZBs for the given parameters
+func (c Client) SearchWithTVDB(categories []int, tvDBID int, season int, episode int) ([]NZB, error) {
+	return c.search(url.Values{
+		"tvdbid":  []string{strconv.Itoa(tvDBID)},
+		"cat":     c.splitCats(categories),
+		"season":  []string{strconv.Itoa(season)},
+		"episode": []string{strconv.Itoa(episode)},
+		"t":       []string{"tvsearch"},
+	})
+}
+
+// SearchWithTVMaze returns NZBs for the given parameters
+func (c Client) SearchWithTVMaze(categories []int, tvMazeID int, season int, episode int) ([]NZB, error) {
+	return c.search(url.Values{
+		"tvmazeid": []string{strconv.Itoa(tvMazeID)},
+		"cat":      c.splitCats(categories),
+		"season":   []string{strconv.Itoa(season)},
+		"episode":  []string{strconv.Itoa(episode)},
+		"t":        []string{"tvsearch"},
+	})
+}
+
+// SearchWithIMDB returns NZBs for the given parameters
+func (c Client) SearchWithIMDB(categories []int, imdbID string) ([]NZB, error) {
+	return c.search(url.Values{
+		"imdbid": []string{imdbID},
+		"cat":    c.splitCats(categories),
+		"t":      []string{"movie"},
+	})
+}
+
+// SearchWithQuery returns NZBs for the given parameters
+func (c Client) SearchWithQuery(categories []int, query string, searchType string) ([]NZB, error) {
+	return c.search(url.Values{
+		"q":   []string{query},
+		"cat": c.splitCats(categories),
+		"t":   []string{searchType},
+	})
+}
+
+// LoadRSSFeed returns up to <num> of the most recent NZBs of the given categories.
+func (c Client) LoadRSSFeed(categories []int, num int) ([]NZB, error) {
+	return c.rss(url.Values{
+		"num": []string{strconv.Itoa(num)},
+		"t":   c.splitCats(categories),
+		"dl":  []string{"1"},
+	})
+}
+
+// Capabilities returns the capabilities of this tracker
+func (c Client) Capabilities() (Capabilities, error) {
+	return c.caps(url.Values{
+		"t": []string{"caps"},
+	})
+}
+
+// LoadRSSFeedUntilNZBID fetches NZBs until a given NZB id is reached.
+func (c Client) LoadRSSFeedUntilNZBID(categories []int, num int, id string, maxRequests int) ([]NZB, error) {
+	count := 0
+	var nzbs []NZB
+	for {
+		partition, err := c.rss(url.Values{
+			"num":    []string{strconv.Itoa(num)},
+			"t":      c.splitCats(categories),
+			"dl":     []string{"1"},
+			"offset": []string{strconv.Itoa(num * count)},
+		})
+		count++
+		if err != nil {
+			return nil, err
+		}
+		for k, nzb := range partition {
+			if nzb.ID == id {
+				return append(nzbs, partition[:k]...), nil
+			}
+		}
+		nzbs = append(nzbs, partition...)
+		if maxRequests != 0 && count == maxRequests {
+			break
+		}
+	}
+	return nzbs, nil
+}
+
+// Details get the details of a particular nzb
+func (c Client) Details(guid string) (Details, error) {
+	return c.details(url.Values{
+		"t":    []string{"details"},
+		"guid": []string{guid},
+	})
+}
+
+func (c Client) splitCats(cats []int) []string {
+	var categories, catsOut []string
+	for _, v := range cats {
+		categories = append(categories, strconv.Itoa(v))
+	}
+	catsOut = append(catsOut, strings.Join(categories, ","))
+	return catsOut
+}
+
+func (c Client) rss(vals url.Values) ([]NZB, error) {
+	vals.Set("r", c.apikey)
+	vals.Set("i", strconv.Itoa(c.apiUserID))
+	return c.process(vals, rssPath)
+}
+
+func (c Client) search(vals url.Values) ([]NZB, error) {
+	vals.Set("apikey", c.apikey)
+	return c.process(vals, apiPath)
+}
+
+func (c Client) caps(vals url.Values) (Capabilities, error) {
+	vals.Set("apikey", c.apikey)
+	resp, err := c.getURL(c.buildURL(vals, apiPath))
+	if err != nil {
+		return Capabilities{}, errors.Wrap(err, "failed to get capabilities")
+	}
+	var cResp Capabilities
+	if err = xml.Unmarshal(resp, &cResp); err != nil {
+		return cResp, errors.Wrap(err, "failed to unmarshal xml")
+	}
+	return cResp, nil
+}
+
+func (c Client) details(vals url.Values) (Details, error) {
+	vals.Set("apikey", c.apikey)
+	resp, err := c.getURL(c.buildURL(vals, apiPath))
+	if err != nil {
+		return Details{}, errors.Wrap(err, "failed to get details")
+	}
+	var dResp Details
+	if err = xml.Unmarshal(resp, &dResp); err != nil {
+		return dResp, errors.Wrap(err, "failed to unmarshal xml")
+	}
+	return dResp, nil
+}
+
+func (c Client) process(vals url.Values, path string) ([]NZB, error) {
+	var nzbs []NZB
+	resp, err := c.getURL(c.buildURL(vals, path))
+	if err != nil {
+		return nzbs, err
+	}
+	var feed SearchResponse
+	err = xml.Unmarshal(resp, &feed)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal xml feed")
+	}
+	if feed.ErrorCode != 0 {
+		return nil, errors.Errorf("newznab api error %d: %s", feed.ErrorCode, feed.ErrorDesc)
+	}
+	for _, gotNZB := range feed.Channel.NZBs {
+		nzb := NZB{
+			Title:          gotNZB.Title,
+			Description:    gotNZB.Description,
+			PubDate:        gotNZB.Date.Add(0),
+			DownloadURL:    gotNZB.Enclosure.URL,
+			SourceEndpoint: c.apiBaseURL,
+			SourceAPIKey:   c.apikey,
+		}
+		for _, attr := range gotNZB.Attributes {
+			switch attr.Name {
+			case "tvairdate":
+				if parsedAirDate, err := parseDate(attr.Value); err != nil {
+					log.WithError(err).WithField("tvairdate", attr.Value).Debug("newznab:Client:Search: failed to parse tvairdate")
+				} else {
+					nzb.AirDate = parsedAirDate
+				}
+			case "guid":
+				nzb.ID = attr.Value
+			case "size":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 64)
+				nzb.Size = parsedInt
+			case "grabs":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.NumGrabs = int(parsedInt)
+			case "comments":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.NumComments = int(parsedInt)
+			case "seeders":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.Seeders = int(parsedInt)
+				nzb.IsTorrent = true
+			case "peers":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.Peers = int(parsedInt)
+				nzb.IsTorrent = true
+			case "infohash":
+				nzb.InfoHash = attr.Value
+				nzb.IsTorrent = true
+			case "category":
+				nzb.Category = append(nzb.Category, attr.Value)
+			case "genre":
+				nzb.Genre = attr.Value
+			case "tvdbid":
+				nzb.TVDBID = attr.Value
+			case "rageid":
+				nzb.TVRageID = attr.Value
+			case "tvmazeid":
+				nzb.TVMazeID = attr.Value
+			case "info":
+				nzb.Info = attr.Value
+			case "season":
+				nzb.Season = attr.Value
+			case "episode":
+				nzb.Episode = attr.Value
+			case "tvtitle":
+				nzb.TVTitle = attr.Value
+			case "rating":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.Rating = int(parsedInt)
+			case "imdb":
+				nzb.IMDBID = attr.Value
+			case "imdbtitle":
+				nzb.IMDBTitle = attr.Value
+			case "imdbyear":
+				parsedInt, _ := strconv.ParseInt(attr.Value, 0, 32)
+				nzb.IMDBYear = int(parsedInt)
+			case "imdbscore":
+				parsedFloat, _ := strconv.ParseFloat(attr.Value, 32)
+				nzb.IMDBScore = float32(parsedFloat)
+			case "coverurl":
+				nzb.CoverURL = attr.Value
+			case "usenetdate":
+				if parsedUsetnetDate, err := parseDate(attr.Value); err != nil {
+					log.WithError(err).WithField("usenetdate", attr.Value).Debug("failed to parse usenetdate")
+				} else {
+					nzb.UsenetDate = parsedUsetnetDate
+				}
+			case "resolution":
+				nzb.Resolution = attr.Value
+			default:
+				log.WithFields(log.Fields{
+					"name":  attr.Name,
+					"value": attr.Value,
+				}).Debug("encontered unknown attribute")
+			}
+		}
+		if nzb.Size == 0 {
+			nzb.Size = gotNZB.Size
+		}
+		nzbs = append(nzbs, nzb)
+	}
+	return nzbs, nil
+}
+
+// PopulateComments fills in the Comments for the given NZB
+func (c Client) PopulateComments(nzb *NZB) error {
+	data, err := c.getURL(c.buildURL(url.Values{
+		"t":      []string{"comments"},
+		"id":     []string{nzb.ID},
+		"apikey": []string{c.apikey},
+	}, apiPath))
+	if err != nil {
+		return err
+	}
+	var resp commentResponse
+	err = xml.Unmarshal(data, &resp)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshal comments xml data")
+	}
+
+	for _, rawComment := range resp.Channel.Comments {
+		comment := Comment{
+			Title:   rawComment.Title,
+			Content: rawComment.Description,
+		}
+		if parsedPubDate, err := time.Parse(time.RFC1123Z, rawComment.PubDate); err != nil {
+			log.WithError(err).WithField("pubdate", rawComment.PubDate).Debug("failed to parse comment date")
+		} else {
+			comment.PubDate = parsedPubDate
+		}
+		nzb.Comments = append(nzb.Comments, comment)
+	}
+	return nil
+}
+
+// NZBDownloadURL returns a URL to download the NZB from
+func (c Client) NZBDownloadURL(nzb NZB) (string, error) {
+	return c.buildURL(url.Values{
+		"t":      []string{"get"},
+		"id":     []string{nzb.ID},
+		"apikey": []string{c.apikey},
+	}, apiPath)
+}
+
+// DownloadNZB returns the bytes of the actual NZB file for the given NZB
+func (c Client) DownloadNZB(nzb NZB) ([]byte, error) {
+	return c.getURL(c.NZBDownloadURL(nzb))
+}
+
+func (c Client) getURL(url string, err error) ([]byte, error) {
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.client.Get(url)
+	if err != nil {
+		return nil, errors.Wrapf(err, "http request failed: %s", url)
+	}
+
+	var data []byte
+	data, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read response body")
+	}
+	return data, nil
+}
+
+func (c Client) buildURL(vals url.Values, path string) (string, error) {
+	parsedURL, err := url.Parse(c.apiBaseURL + path)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to parse base API url")
+	}
+
+	parsedURL.RawQuery = vals.Encode()
+	return parsedURL.String(), nil
+}
+
+func parseDate(date string) (time.Time, error) {
+	formats := []string{time.RFC3339, time.RFC1123Z}
+	var parsedTime time.Time
+	var err error
+	for _, format := range formats {
+		if parsedTime, err = time.Parse(format, date); err == nil {
+			return parsedTime, nil
+		}
+	}
+	return parsedTime, errors.Errorf("failed to parse date %s as one of %s", date, strings.Join(formats, ", "))
+}
+
+const (
+	apiPath = "/api"
+	rssPath = "/rss"
+)
+
+type commentResponse struct {
+	Channel struct {
+		Comments []rssComment `xml:"item"`
+	} `xml:"channel"`
+}
+
+type rssComment struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
---
+++ newznab/newznab_test.go
@@ -0,0 +1,189 @@
+package newznab
+
+// fork of https://github.com/mrobinsn/go-newznab/tree/master/newznab
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUsenetCrawlerClient(t *testing.T) {
+	//log.SetLevel(log.DebugLevel)
+	apiKey := "gibberish"
+
+	// Set up our mock server
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var f []byte
+		var err error
+
+		reg := regexp.MustCompile(`\W`)
+		fixedPath := reg.ReplaceAllString(r.URL.RawQuery, "_")
+
+		if r.URL.Query()["t"][0] == "get" {
+			// Fetch nzb
+			nzbID := r.URL.Query()["id"][0]
+			filePath := fmt.Sprintf("../tests/fixtures/nzbs/%v.nzb", nzbID)
+			f, err = ioutil.ReadFile(filePath)
+		} else {
+			// Get xml
+			filePath := fmt.Sprintf("../tests/fixtures%v/%v.xml", r.URL.Path, fixedPath)
+			f, err = ioutil.ReadFile(filePath)
+		}
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("File not found")) // nolint:errcheck
+		} else {
+			w.Write(f) // nolint:errcheck
+		}
+	}))
+
+	defer ts.Close()
+
+	t.Run("torznab client", func(t *testing.T) {
+		client := New(ts.URL, apiKey, 1234, true)
+
+		t.Run("Simple query search", func(t *testing.T) {
+			categories := []int{CategoryTVHD}
+			results, err := client.SearchWithQuery(categories, "Supernatural S11E01", "tvshows")
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+		})
+	})
+
+	t.Run("nzb client", func(t *testing.T) {
+		client := New(ts.URL, apiKey, 1234, false)
+		categories := []int{CategoryTVSD}
+
+		t.Run("invalid search", func(t *testing.T) {
+			_, err := client.SearchWithTVDB(categories, 1234, 9, 2)
+			require.Error(t, err, "expected an error")
+		})
+
+		t.Run("invalid api usage", func(t *testing.T) {
+			_, err := client.SearchWithTVDB(categories, 5678, 9, 2)
+			require.Error(t, err, "expected an error")
+			require.EqualError(t, err, "newznab api error 100: Invalid API Key")
+		})
+
+		t.Run("valid category and TheTVDB id", func(t *testing.T) {
+			results, err := client.SearchWithTVDB(categories, 75682, 10, 1)
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+		})
+
+		t.Run("valid category and TVMaze id", func(t *testing.T) {
+			results, err := client.SearchWithTVMaze(categories, 65, 10, 1)
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+		})
+
+		t.Run("valid category and tvrage id", func(t *testing.T) {
+			results, err := client.SearchWithTVRage(categories, 2870, 10, 1)
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+
+			t.Run("populate comments", func(t *testing.T) {
+				nzb := results[1]
+				require.Empty(t, nzb.Comments)
+				require.NotZero(t, nzb.NumComments)
+				err := client.PopulateComments(&nzb)
+				require.NoError(t, err)
+				require.NotEmpty(t, nzb.Comments, "expected at least one comment")
+				for _, comment := range nzb.Comments {
+					require.NotEmpty(t, comment, "comment should not be empty")
+				}
+			})
+
+			t.Run("download url", func(t *testing.T) {
+				url, err := client.NZBDownloadURL(results[0])
+				require.NoError(t, err)
+				require.NotEmpty(t, url, "expected a url")
+			})
+
+			t.Run("download nzb", func(t *testing.T) {
+				bytes, err := client.DownloadNZB(results[0])
+				require.NoError(t, err)
+				require.NotEmpty(t, bytes, "expected to download something")
+			})
+		})
+
+		t.Run("multiple categories and IMDB id", func(t *testing.T) {
+			cats := []int{
+				CategoryMovieHD,
+				CategoryMovieBluRay,
+			}
+			results, err := client.SearchWithIMDB(cats, "0371746")
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+
+			require.Equal(t, "2040", results[0].Category[1])
+			require.Equal(t, "2050", results[22].Category[1])
+		})
+
+		t.Run("single category and IMDB id", func(t *testing.T) {
+			cats := []int{CategoryMovieHD}
+			results, err := client.SearchWithIMDB(cats, "0364569")
+			require.NoError(t, err)
+			require.NotEmpty(t, results, "expected results")
+
+			t.Run("movie specific fields", func(t *testing.T) {
+				require.Equal(t, "0364569", results[0].IMDBID)
+				require.Equal(t, "Oldboy", results[0].IMDBTitle)
+				require.Equal(t, 2003, results[0].IMDBYear)
+				require.Equal(t, float32(8.4), results[0].IMDBScore)
+				require.Equal(t, "https://dognzb.cr/content/covers/movies/thumbs/364569.jpg", results[0].CoverURL)
+			})
+		})
+
+		t.Run("recent items via RSS", func(t *testing.T) {
+			num := 50
+			categories := []int{CategoryMovieAll, CategoryTVAll}
+
+			t.Run("recent items", func(t *testing.T) {
+				results, err := client.LoadRSSFeed(categories, num)
+				require.NoError(t, err)
+				require.Len(t, results, num)
+				require.Equal(t, "bcdbf3f1e7a1ef964527f1d40d5ec639", results[0].ID)
+				require.Equal(t, "030517-VSHS0101720WDA20H264V", results[6].Title)
+
+				t.Run("airdate with RFC1123Z format", func(t *testing.T) {
+					require.Equal(t, 2017, results[7].AirDate.Year())
+				})
+
+				t.Run("usenetdate with RFC3339 format", func(t *testing.T) {
+					require.Equal(t, 2017, results[7].UsenetDate.Year())
+				})
+			})
+
+			t.Run("up until", func(t *testing.T) {
+				results, err := client.LoadRSSFeedUntilNZBID(categories, num, "29527a54ac54bb7533abacd7dad66a6a", 0)
+				require.NoError(t, err)
+				require.Len(t, results, 101)
+
+				t.Run("boundary results", func(t *testing.T) {
+					require.Equal(t, "8841b21c4d2fb96f0d47ca24cae9a5b7", results[0].ID)
+					require.Equal(t, "2c6c0e2ac562db69d8b3646deaf2d0cd", results[len(results)-1].ID)
+				})
+
+				t.Run("RSS up until with failures/retries", func(t *testing.T) {
+					results, err := client.LoadRSSFeedUntilNZBID(categories, num, "does-not-exist", 2)
+					require.NoError(t, err)
+					require.Len(t, results, 100)
+				})
+			})
+		})
+
+		t.Run("single nzb details", func(t *testing.T) {
+			d, err := client.Details("4694b91a86adc4ebd3b289687ebf4b0d")
+			require.NoError(t, err)
+			require.Equal(t, "Car.Craft-July.2015", d.Channel.Item.Title)
+		})
+	})
+}
