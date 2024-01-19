edf05a4f23f7f4f1997ef0ee76ca0158f3c5e896
Shawn Catanzarite <me@shawncatz.com>
2024-01-18 21:39:49 -0800

    fill out all of the searching caps

--- source.go
+++ source.go
@@ -117,10 +117,22 @@ func newznabToJackett(i newznab.Capabilities) *jackett.Capabilities {
 	caps := &jackett.Capabilities{}
 	caps.Searching.Search.Available = i.Searching.Search.Available
 	caps.Searching.Search.SupportedParams = i.Searching.Search.SupportedParams
+	caps.Searching.Search.SearchEngine = "raw"
 	caps.Searching.TvSearch.Available = i.Searching.Search.Available
 	caps.Searching.TvSearch.SupportedParams = i.Searching.Search.SupportedParams
+	caps.Searching.TvSearch.SearchEngine = "raw"
 	caps.Searching.MovieSearch.Available = i.Searching.Search.Available
 	caps.Searching.MovieSearch.SupportedParams = i.Searching.Search.SupportedParams
+	caps.Searching.MovieSearch.SearchEngine = "raw"
+	caps.Searching.MusicSearch.Available = "no"
+	caps.Searching.MusicSearch.SupportedParams = ""
+	caps.Searching.MusicSearch.SearchEngine = "raw"
+	caps.Searching.AudioSearch.Available = "no"
+	caps.Searching.AudioSearch.SupportedParams = ""
+	caps.Searching.AudioSearch.SearchEngine = "raw"
+	caps.Searching.BookSearch.Available = "no"
+	caps.Searching.BookSearch.SupportedParams = ""
+	caps.Searching.BookSearch.SearchEngine = "raw"
 
 	for _, cat := range i.Categories.Category {
 		c := jackett.Category{
