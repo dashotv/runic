b530a6fe072eec3ab1bc41136c337d3389ed29e9
Shawn Catanzarite <me@shawncatz.com>
2024-03-07 15:27:06 -0800

    component federation: pull runic embedded ui into dashotv/ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
+import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    federation({
+      name: 'runic',
+      filename: 'remote.js',
+      exposes: {
+        './Releases': './src/pages/releases.tsx',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ],
   build: {
     outDir: '../static',
   },
