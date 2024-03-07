11addbbc749d946180f19d2e054d9aa911238a15
Shawn Catanzarite <me@shawncatz.com>
2024-03-07 04:48:48 -0800

    add embedded ui, going to try module federation with dashotv/ui

---
+++ app/routes_static.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/dashotv/runic/static"
+)
+
+func init() {
+	initializers = append(initializers, setupStatic)
+}
+
+func setupStatic(a *Application) error {
+	app.Default.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Index:      "index.html", // This is the default html page for your SPA
+		Browse:     false,
+		HTML5:      true,
+		Filesystem: http.FS(static.FS),
+	})) // https://echo.labstack.com/docs/middleware/static
+	return nil
+}
---
+++ ui/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../static",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9002",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
